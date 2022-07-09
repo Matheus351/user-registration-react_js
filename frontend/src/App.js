8390fdc5696a7d121225d1868cf98031de244f14
Matheus Valença <matheusniklaus@gmail.com>
2022-07-08 21:38:00 -0300

    criando componentes base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Logo from './components/template/Logo'
+import Nav from './components/template/Nav'
+import Main from './components/template/Main'
+import Footer from './components/template/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo/>
+      <Nav/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
