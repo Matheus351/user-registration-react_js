ca56fbc0bbca953ab922f444222bfa0b8eccc994
Matheus Valença <matheusniklaus@gmail.com>
2022-07-10 15:44:55 -0300

    adicionando responsividade

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default props =>
   <aside className='menu-area'>
-    <nav className='menu-area'>
+    <nav className='menu'>
       <Link to='/'>
         <i className='fa fa-home'></i> Home
       </Link>
